369d8e3f7a4e463ef24599455ef24c6546e25f1c
AliHosseinPour85 <ali380h641p@gmail.com>
2025-03-22 15:09:51 +0330

    remove bug

--- GPS/gps.cpp
+++ GPS/gps.cpp
@@ -1,16 +1,20 @@
 #include <gps.hpp>
 #include <fstream>
 
+int Position::n = 0;
+
 Position::Position(double lat, double lon)
 {
     if (lat < -90.0 || lat > 90.0)
     {
         std::cerr << "Latitude must be between -90 and 90." << std::endl;
+        latitude = 1000;//it use in destructor
         return;
     }
     if (lon < -180.0 || lon > 180.0)
     {
         std::cerr << "Longitude must be between -180 and 180." << std::endl;
+        longitude = 1000;//it use in destructor
         return;
     }
 
@@ -18,9 +22,16 @@ Position::Position(double lat, double lon)
     longitude = lon;
 
     std::cout << "Latitude: " << latitude << ", Longitude: " << longitude << std::endl;
+    n++;
 }
 
-Position::~Position(){
+Position::~Position()
+{
+    if (latitude == 1000 || longitude == 1000) 
+    {
+        return;//it use when a position is incorrect and this if, skip it.
+    }
+
     if (n > 0)
     {
         std::ofstream file("log.txt");

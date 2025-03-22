cda24ff77dbbd4633ab1b4b73ba9ecdea4191441
AliHosseinPour85 <ali380h641p@gmail.com>
2025-03-22 16:13:44 +0330

    add game.hpp and writing Game class

---
+++ GameCharacter/game.hpp
@@ -0,0 +1,16 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include "character.hpp"
+
+
+class Game {
+private:
+    std::vector<Character> characters;
+
+public:
+    void addCharacter(const Character& character);
+
+};
+
+#endif

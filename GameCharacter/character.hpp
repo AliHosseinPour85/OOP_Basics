56baf890c1ec6ce8abcd2f5402a6ffab59ab0674
AliHosseinPour85 <ali380h641p@gmail.com>
2025-03-22 15:36:34 +0330

    add character.hpp and writing character class

---
+++ GameCharacter/character.hpp
@@ -0,0 +1,21 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include <vector>
+#include <iostream>
+
+class Character {
+private:
+    std::string Name;
+    int Health;
+    int Power;
+
+    std::vector<std::string> SkillNames;
+    std::vector<int> SkillLevels;
+
+public:
+    Character(const std::string& name, int health, int power);
+    void addSkill(const std::string& skillName, int level);
+};
+
+#endif

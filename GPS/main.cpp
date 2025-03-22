099254350933d178e57d50737affb216e5f6dff6
AliHosseinPour85 <ali380h641p@gmail.com>
2025-03-22 14:53:05 +0330

    add main and CMakeLists for compile

---
+++ GPS/main.cpp
@@ -0,0 +1,11 @@
+#include "gps.hpp"
+
+int main()
+{
+    Position position1(181, 45.6);
+    Position position2(12.3, -76.4);
+    Position position3(-23.8, 189.1);
+    Position position4(-2.38, 89.1);
+
+    return 0;
+}

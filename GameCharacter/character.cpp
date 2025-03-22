9626731f77652b4fe41c250510f2fef7115f05db
AliHosseinPour85 <ali380h641p@gmail.com>
2025-03-22 17:19:05 +0330

    remove some bugs and add CMakeLists.txt for compile

--- GameCharacter/character.cpp
+++ GameCharacter/character.cpp
@@ -4,6 +4,16 @@
 Character::Character(const std::string &name, int health, int power)
 {
     Name = name;
+    while ( health <= 0 )
+    {
+        std::cerr << "Enter a Health more than 0 !!!";
+        std::cin >> health;
+    } 
+    while ( power <= 0 )
+    {
+        std::cerr << "Enter a Power more than 0 !!!";
+        std::cin >> power;
+    }  
     Health = health;
     Power = power;
 
@@ -12,6 +22,11 @@ Character::Character(const std::string &name, int health, int power)
 void Character::addSkill(const std::string &skillName, int level)
 {
     SkillNames.push_back(skillName);
+    while ( level <= 0 )
+    {
+        std::cerr << "Enter a Level more than 0 !!!";
+        std::cin >> level;        
+    } 
     SkillLevels.push_back(level);
 }
 
